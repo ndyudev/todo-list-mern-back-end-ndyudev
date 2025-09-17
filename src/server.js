d829c771af79cdc5e345059079b0baf1f9821c7a
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-09-17 23:43:32 +0700

    Config CORS, Filter Data aggeragete

--- src/server.js
+++ src/server.js
@@ -2,14 +2,16 @@ import express from "express";
 import tasksRouters from "./routers/tasksRouters.js";
 import { connectDB } from "./config/database.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const PORT = process.env.PORT_TODO_LIST || 5001;
 
 const app = express();
-
+// middlewares
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173" }));
 
 app.use("/api/tasks", tasksRouters);
 
